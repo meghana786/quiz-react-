f4644f9d81b29224c2ad00779fcccce22e49302f
meghana786 <160600002+meghana786@users.noreply.github.com>
2024-05-22 17:48:21 +0530

    Create HomeComponent.jsx

---
+++ src/components/HomeComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css'
+
+class HomeComponent extends Component {
+  render() {
+    return (
+      <div className='home'>
+        <h1>Quiz App</h1>
+        <button>Play</button>
+      </div>
+    );
+  }
+}
+
+export default HomeComponent;
