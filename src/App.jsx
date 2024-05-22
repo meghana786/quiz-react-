84a1bcdfc8e2b5d06ca21c8b441f628d15ca8ab0
meghana786 <160600002+meghana786@users.noreply.github.com>
2024-05-22 17:47:36 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HomeComponent from '../src/components/HomeComponent';
+import QuizComponent from '../src/components/QuizComponent';
+import ResultComponent from '../src/components/ResultComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <QuizComponent/>
+    </div>
+  );
+}
+
+export default App;
